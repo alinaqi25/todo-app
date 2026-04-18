b1fe96d9078334e71af99f96e540e39d3f58e2f0
Ali Naqi <alinaqikhoso25@gmail.com>
2026-04-18 12:06:57 +0500

    Basic backend login and register setup

---
+++ src/routes/todoRoutes.js
@@ -0,0 +1,22 @@
+import express from 'express'
+import db from '../db.js'
+
+const router = express.Router()
+
+// get or post all the todos from an authenticated user
+
+router.get('/', (req, res)=>{
+
+})
+
+router.post('/', (req, res)=>{
+
+})
+
+// edit a todo
+router.put('/', (req, res)=>{
+
+})
+
+
+export default router
